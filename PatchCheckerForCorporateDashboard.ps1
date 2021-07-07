10437d1226270513ff807413aa83f4b60ac07ab1
Benjamin Fridkis <32713413+bfridkis@users.noreply.github.com>
2021-07-07 09:50:26 -0500

    Add files via upload

---
+++ PatchCheckerForCorporateDashboard.ps1
@@ -0,0 +1,14 @@
+﻿Clear-Host
+
+Write-Output "`n`t`t`t`t`t*^*!*% Patch Checker for Corporate Dashboard *^*!*% "
+
+Remove-Item E:\PatchCheckerForCorporateDashboard\PatchCheckerForCorporateDashboard_ErrorLog*
+
+$patcheckerOutputFileName = "E:\PatchCheckerForCorporateDashboard\PatchCheckerOutput_CorporateDashboard_$(if ((Get-Date).Month -eq 1) { "12_$((Get-Date).Year - 1)" } else { "$($(Get-Date).Month - 1)_$((Get-Date).Year)" })"
+#$patcheckerOutputFileName = "E:\PatchCheckerForCorporateDashboard\PatchCheckerOutput_CorporateDashboard_$(Get-Date -Format MMddyyyy_HHmmss)"
+
+E:\PatchCheckerForCorporateDashboard\PatchCheckerV2.5.ps1 -Option 3 -AllNodes TRUE -OutputMode 1 `
+-OutputFile "$patcheckerOutputFileName" -CheckComplianceAsOfDate Today -ForCorporateDashboard "TRUE"
+
+#Send-MailMessage -From PCNSMS04@wmgpcn.local -To ben.j.fridkis@p66.com -Subject "PatchCheckerOutput_CorporateDashboard_$(if ((Get-Date).Month -eq 1) { "12_$((Get-Date).Year - 1)" } else { "$($(Get-Date).Month - 1)_$((Get-Date).Year)" })"  `
+#                 -Attachments $patcheckerOutputFileName -SmtpServer 164.123.219.98
